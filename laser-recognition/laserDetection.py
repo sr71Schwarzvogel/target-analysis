89f36102127a5ef185c6d64c9603282b68ead728
Max <maximilian.harth@gmx.de>
2018-05-28 16:07:37 +0200

    the movements of the laser are now shown on the video

--- laser-recognition/laserDetection.py
+++ laser-recognition/laserDetection.py
@@ -2,42 +2,28 @@ import cv2
 import numpy as np
 import math
 
-
-x_old = -1
-y_old = -1
-
-# Read image
-draw_pic = cv2.imread("test-pictures/laser-test-picture-4.jpg", cv2.IMREAD_GRAYSCALE)
+nonzero = [1]
+laserTrack = []
 
 # Read Video
 cap = cv2.VideoCapture('test-pictures/laser-test-video-1.mp4')
-nonzero = [1]
+
 videoRunning = True
 while (videoRunning):
     try:
         _, im = cap.read()
         im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
-
-
         mask = cv2.inRange(im, 220, 255)
 
         # cleaning the mask
-        #closing
+        # closing
         kernel = np.ones((3, 3), np.uint8)
         dilation = cv2.dilate(mask, kernel, iterations=1)
 
         # opening
         kernel = np.ones((7, 7), np.uint8)
         cleaned_mask = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel)
-
-        # opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-        # closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-
-        # erosion = cv2.erode(mask,kernel,iterations = 1)
-        # dilation = cv2.dilate(mask,kernel,iterations = 1)
-
-        # TODO draw the locations of the laser (connections between blobs? or white points)
 
         # get middle coordinates of white
         nonzero = np.argwhere(cleaned_mask == 255)
@@ -46,39 +32,40 @@ while (videoRunning):
         x = 0
         y = 0
         z_counter = 1
-        print(nonzero)
+
         for z in nonzero:
             x_sum = x_sum + z[0]
             y_sum = y_sum + z[1]
             z_counter = z_counter + 1
 
+        if x_sum != 0:
+            z_counter = z_counter - 1
+            x = x_sum / z_counter
+            y = y_sum / z_counter
 
-        x = x_sum / z_counter
-        y = y_sum / z_counter
+            # round up coordinates
+            x = math.ceil(x)
+            y = math.ceil(y)
 
-        # round up coordinates
-        x = math.ceil(x)
-        y = math.ceil(y)
+            laserTrack.append([x, y])
 
         # draw line on image
-        if x_old != -1:
-            cv2.line(cleaned_mask, (x, y), (x_old, y_old), (0, 255, 0), 2)
-
-        
-        x_old = x
-        y_old = y
-
+        s = len(laserTrack)
+        if s > 2:
+            for i in range(1, s):
+                p1 = laserTrack[i-1]
+                p2 = laserTrack[i]
+                cv2.line(im, (p1[1], p1[0]), (p2[1], p2[0]), (255, 0, 0), 2)
 
         cv2.imshow("original", im)
-        cv2.imshow("cleaned_mask", cleaned_mask)
-        #cv2.imshow("movement", draw_pic)
-
+        # cv2.imshow("cleaned_mask", cleaned_mask)
+        # cv2.imshow("movement", draw_pic)
 
         k = cv2.waitKey(5) & 0xFF
         if k == 27:
             break
 
-
     except cv2.error:
         videoRunning = False
+
 
