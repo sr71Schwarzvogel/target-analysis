02c03b31ba2e7cc4df860b20e0718220b0cb42d6
Max <maximilian.harth@gmx.de>
2018-06-20 16:31:15 +0200

    continued to work on movementDetection to detect the bullet holes

--- hit-recognition/movementDetection.py
+++ hit-recognition/movementDetection.py
@@ -16,17 +16,43 @@ while (1):
 
     # cleaning the mask
     # closing
-    kernel = np.ones((3, 3), np.uint8)
-    dilation = cv2.dilate(fgmask, kernel, iterations=1)
+    #kernel = np.ones((3, 3), np.uint8)
+    #dilation = cv2.dilate(fgmask, kernel, iterations=1)
 
     # dilation
-    kernel = np.ones((7, 7), np.uint8)
-    cleaned_mask = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel)
+    #kernel = np.ones((7, 7), np.uint8)
+    #cleaned_mask = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel)
 
+    # opening
+    kernel = np.ones((7, 7), np.uint8)
+    cleaned_mask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+
+    # smoothing
+    # kernel = np.ones((15, 15), np.float32) / 225
+    # cleaned_mask = cv2.filter2D(fgmask, -1, kernel)
+
+
+
+    #cleaned_mask = fgmask
+
+    # invert image
+    cleaned_mask = cv2.bitwise_not(cleaned_mask)
+
+    # blob detection
+    # Set up the detector with default parameters.
+    detector = cv2.SimpleBlobDetector_create()
+
+    # Detect blobs.
+    keypoints = detector.detect(cleaned_mask)
+
+    # Draw detected blobs as red circles.
+    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+    detected_blobs = cv2.drawKeypoints(cleaned_mask, keypoints, np.array([]), (0, 0, 255),
+                                          cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     cv2.imshow('fgmask', frame)
     cv2.imshow('frame', fgmask)
-    cv2.imshow('cleaned mask', cleaned_mask)
+    cv2.imshow('outcome', detected_blobs)
 
     k = cv2.waitKey(30) & 0xff
     if k == 27:
